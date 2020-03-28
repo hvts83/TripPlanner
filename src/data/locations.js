25ac9ec263fe6cb5a2e890365e8303b043fb1f54
Herbert V. Tejada <herberthtejada@gmail.com>
2020-03-28 00:27:58 -0600

    App Upload

---
+++ src/data/locations.js
@@ -0,0 +1,57 @@
+export const locations = [
+  {
+    placename: 'Yuma, Arizona',
+    date: 'December, 2015',
+    location: {
+      lat: 32.698437,
+      lng: -114.650398
+    },
+    todo: [
+      'Work'
+    ]
+  },
+  {
+    placename: 'New York',
+    date: 'January, 2016',
+    location: {
+      lat: 40.789143,
+      lng: -73.134964
+    },
+    todo: [
+      'Family'
+    ]
+  },
+  {
+    placename: 'El Salvador',
+    date: 'February, 2016',
+    location: {
+      lat: 13.794185,
+      lng: -88.896530
+    },
+    todo: [
+      'Home',
+    ]
+  },
+  {
+    placename: 'Miami',
+    date: 'September, 2019',
+    location: {
+      lat: 25.761681,
+      lng: -80.191788
+    },
+    todo: [
+      'Layover'
+    ]
+  },
+  {
+    placename: 'New York',
+    date: 'September, 2019',
+    location: {
+      lat: 40.789143,
+      lng: -73.134964
+    },
+    todo: [
+      'Study'
+    ]
+  }
+];
